7d7611a4164f6d158b77c87b846918fbd09cdfa8
PaulDavydov <pauldvyd@gmail.com>
2024-11-20 20:51:03 -0800

    add additional configs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,21 +2,28 @@ package main
 import (
   "log"
   "net/http"
+  "flag"
 )
 
 func main() {
+
+  // Hardcodes the command line flag with the name addr.
+  addr := flag.String("addr", ":4000", "HTTP network address")
+
+  flag.Parse()
+
   mux := http.NewServeMux()
   
   // Serves files out of the static directory
   fileServer := http.FileServer(http.Dir("./ui/static/"))
-
   mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
   mux.HandleFunc("/", home)
   mux.HandleFunc("/snippet/view", snippetView)
   mux.HandleFunc("/snippet/create", snippetCreate)
 
-  log.Println("Starting the server on :4000")
-  err := http.ListenAndServe(":4000", mux)
+  // Add address passed in as argument to be printed here
+  log.Printf("Starting server on %s", *addr)
+  err := http.ListenAndServe(*addr, mux)
   log.Fatal(err)
 }
