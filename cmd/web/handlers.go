7e7fe73297e014aed90621fa985cf59418da1f84
PaulDavydov <pauldvyd@gmail.com>
2025-02-24 20:56:46 -0800

    refactoring and error logging

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,55 +1,52 @@
 package main
 
 import (
-  "fmt"
-  "html/template"
-  "log"
-  "net/http"
-  "strconv"
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-  if r.URL.Path != "/" {
-    http.NotFound(w,r)
-    return
-  }
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		app.notFound(w)
+		return
+	}
 
-  files := []string{
-    "./ui/html/base.tmpl",
-    "./ui/html/pages/home.tmpl",
-    "./ui/html/partials/nav.tmpl",
-  }
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.tmpl",
+		"./ui/html/partials/nav.tmpl",
+	}
 
-  ts, err := template.ParseFiles(files...)
-  if err != nil {
-    log.Println(err.Error())
-    http.Error(w, "Internal Server Error", 500)
-    return
-  }
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
 
-  err = ts.ExecuteTemplate(w, "base", nil)
-  if err != nil {
-    log.Println(err.Error())
-    http.Error(w, "Internal Server Error", 500)
-  }
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		app.serverError(w, err)
+	}
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
-  id, err := strconv.Atoi(r.URL.Query().Get("id"))
-  if err != nil || id < 1 {
-    http.NotFound(w,r)
-    return
-  }
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
 
-  fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
-  if r.Method != http.MethodPost {
-    w.Header().Set("Allow", http.MethodPost)
-    http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-    return
-  }
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
 
-  w.Write([]byte("Create a new snippet..."))
+	w.Write([]byte("Create a new snippet..."))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,41 +1,41 @@
 package main
+
 import (
-  "log"
-  "net/http"
-  "flag"
-  "os"
+	"flag"
+	"log"
+	"net/http"
+	"os"
 )
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
 
 func main() {
 
-  // Hardcodes the command line flag with the name addr.
-  addr := flag.String("addr", ":4000", "HTTP network address")
+	// Hardcodes the command line flag with the name addr.
+	addr := flag.String("addr", ":4000", "HTTP network address")
 
-  flag.Parse()
+	flag.Parse()
 
-  infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-  errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
 
-  mux := http.NewServeMux()
-  
-  // Serves files out of the static directory
-  fileServer := http.FileServer(http.Dir("./ui/static/"))
-  mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	// Initialize http.Server struct and add ErrorLog field
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
 
-  mux.HandleFunc("/", home)
-  mux.HandleFunc("/snippet/view", snippetView)
-  mux.HandleFunc("/snippet/create", snippetCreate)
-  
-  // Initialize http.Server struct and add ErrorLog field
-  srv := &http.Server{
-    Addr: *addr,
-    ErrorLog: errorLog,
-    Handler: mux,
-  }
-
-  // Add address passed in as argument to be printed here
-  infoLog.Printf("Starting server on %s", *addr)
-  err := srv.ListenAndServe()
-  errorLog.Fatal(err)
+	// Add address passed in as argument to be printed here
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
